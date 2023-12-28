2de470336cf3b86bd3fa06bd9691adc9175aba6d
swasilyev <swasilyev@gmail.com>
2023-12-28 18:11:52 +0300

    constraint tracker

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,38 @@
 mod affine_gen;
 mod apk_circuits;
+
+#[cfg(test)]
+mod tests {
+    use ark_ff::Field;
+    use ark_relations::r1cs::ConstraintSystemRef;
+    use derivative::Derivative;
+
+    #[derive(Derivative)]
+    #[derivative(Debug, Clone)]
+    pub struct Tracker {
+        num_constraints: usize,
+        num_witness_variables: usize,
+        num_instance_variables: usize,
+    }
+
+    impl Tracker {
+        pub fn new<F: Field>(cs: &ConstraintSystemRef<F>) -> Self {
+            Self {
+                num_constraints: cs.num_constraints(),
+                num_witness_variables: cs.num_witness_variables(),
+                num_instance_variables: cs.num_instance_variables(),
+            }
+        }
+
+        pub fn update<F: Field>(&mut self, cs: &ConstraintSystemRef<F>) -> Self {
+            let new = Self::new(cs);
+            let delta = Self {
+                num_constraints: new.num_constraints - self.num_constraints,
+                num_witness_variables: new.num_witness_variables - self.num_witness_variables,
+                num_instance_variables: new.num_instance_variables - self.num_instance_variables,
+            };
+            *self = new;
+            delta
+        }
+    }
+}
